7cecf504fc83e4287571c69341b1af330fedaf3c
pooya-mohammadi <pooyamohammadikazaj@gmail.com>
2024-11-27 21:44:23 +0100

    Update codes

--- data/data_preparation.py
+++ data/data_preparation.py
@@ -4,7 +4,6 @@ import shutil
 import pandas as pd
 from deep_utils import DirUtils
 from os.path import split, join
-from collections import defaultdict
 
 from sklearn.model_selection import train_test_split
 
@@ -13,6 +12,8 @@ output_dir = "../sentiment_data/"
 val_dir = "../sentiment_data/val"
 test_dir = "../sentiment_data/test"
 train_dir = "../sentiment_data/train"
+
+split_type = "class"  # id class
 
 DirUtils.remove_create(output_dir)
 test_size = 0.25
@@ -30,28 +31,45 @@ def move_me(sample_ids, out_dir):
         shutil.copy(sample_path, join(out_dir, audio_cls, audio_name))
 
 
-for folder in data_folders:
-    for sample in DirUtils.list_dir_full_path(folder, interest_extensions=".wav"):
-        name = split(sample)[-1]
-        gender = name[0]
-        sample_id = name[1:3]
-        audio_class = name[3]
-        audio_id = name[4:]
-        if sample_id and sample_id not in data_sample_id:
-            data_sample_id[sample_id] = [(audio_class, name, sample)]
-        elif sample_id and sample_id in data_sample_id:
-            data_sample_id[sample_id].append((audio_class, name, sample))
-        else:
-            raise ValueError(sample_id)
-train_sample_id, val_sample_id = train_test_split(list(data_sample_id.keys()), test_size=test_size)
-val_sample_id, test_sample_id = train_test_split(val_sample_id, test_size=0.5)
+if split_type == "id":
+    for folder in data_folders:
+        for sample in DirUtils.list_dir_full_path(folder, interest_extensions=".wav"):
+            name = split(sample)[-1]
+            gender = name[0]
+            sample_id = name[1:3]
+            audio_class = name[3]
+            audio_id = name[4:]
+            if sample_id and sample_id not in data_sample_id:
+                data_sample_id[sample_id] = [(audio_class, name, sample)]
+            elif sample_id and sample_id in data_sample_id:
+                data_sample_id[sample_id].append((audio_class, name, sample))
+            else:
+                raise ValueError(sample_id)
+    train_sample_id, val_sample_id = train_test_split(list(data_sample_id.keys()), test_size=test_size)
+    val_sample_id, test_sample_id = train_test_split(val_sample_id, test_size=0.5)
 
-move_me([data_sample_id[key] for key in train_sample_id if data_sample_id[key]], train_dir)
-move_me([data_sample_id[key] for key in val_sample_id if data_sample_id[key]], val_dir)
-move_me([data_sample_id[key] for key in test_sample_id if data_sample_id[key]], test_dir)
+    move_me([data_sample_id[key] for key in train_sample_id if data_sample_id[key]], train_dir)
+    move_me([data_sample_id[key] for key in val_sample_id if data_sample_id[key]], val_dir)
+    move_me([data_sample_id[key] for key in test_sample_id if data_sample_id[key]], test_dir)
+elif split_type == "class":
+    for folder in data_folders:
+        for sample in DirUtils.list_dir_full_path(folder, interest_extensions=".wav"):
+            name = split(sample)[-1]
+            gender = name[0]
+            sample_id = name[1:3]
+            audio_class = name[3]
+            audio_id = name[4:]
+            os.makedirs(join(output_dir, audio_class), exist_ok=True)
+            shutil.copy(sample, join(output_dir, audio_class, name))
 
-    # os.makedirs(join(output_dir, audio_class), exist_ok=True)
-    # shutil.copy(sample, join(output_dir, audio_class, name))
+    DirUtils.split_dir_of_dir(output_dir, train_dir=train_dir, val_dir=val_dir + "_val", test_size=test_size, remove_in_dir=True)
+    DirUtils.split_dir_of_dir(val_dir + "_val", train_dir=val_dir, val_dir=test_dir, test_size=0.5, remove_in_dir=True)
+else:
+    raise ValueError()
+
+
+# os.makedirs(join(output_dir, audio_class), exist_ok=True)
+# shutil.copy(sample, join(output_dir, audio_class, name))
 
 
 def save_csv(data_path: str, csv_path: str):
