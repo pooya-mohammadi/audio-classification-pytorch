c55c733abc1c5792defb2db45a6c02a7e4d2c9b9
pooya-mohammadi <pooyamohammadikazaj@gmail.com>
2025-01-11 16:02:32 +0100

    Update codes

--- wav2vec2/evaluate.py
+++ wav2vec2/evaluate.py
@@ -1,68 +1,4 @@
-# import numpy as np
-# import torch
-# from deep_utils import PickleUtils, DirUtils, JsonUtils
-# from transformers import AutoModelForAudioClassification, AutoFeatureExtractor
-# from settings import Config
-# from pathlib import Path
-# import librosa
-# from tqdm import tqdm
-# from sklearn.metrics import f1_score, recall_score, accuracy_score, precision_score
-#
-# sample_path = "../test_data"
-# =======
-# inference_dir = Path("./results/exp_91/best")
-# sample_path = "../sentiment_data/val"
-# >>>>>>> 024406b (Update codes)
-#
-# config = Config()
-# label2id = PickleUtils.load_pickle(inference_dir / "label2id.pkl")
-# id2label = {int(v): k for k, v in label2id.items()}
-# feature_extractor = AutoFeatureExtractor.from_pretrained(config.feature_extractor)
-#
-#
-# def get_audio(path: str):
-#     audio, _ = librosa.load(path)
-#     inputs = feature_extractor(
-#         audio,
-#         sampling_rate=feature_extractor.sampling_rate,
-#         max_length=16000,
-#         truncation=True
-#     )
-#     return inputs['input_values'][0]
-#
-#
-# # early_stopping = EarlyStoppingCallback(early_stopping_patience=config.early_stopping_patience)
-#
-# device = "cuda"
-# model = AutoModelForAudioClassification.from_pretrained(
-#     inference_dir,
-#     num_labels=len(label2id),
-#     label2id=label2id,
-#     id2label=id2label
-# )
-#
-# # model.load_state_dict(torch.load(inference_dir / "model.safetensors"))
-# model = model.to(device).eval()
-#
-# if __name__ == '__main__':
-#     files, true_labels, mapping = DirUtils.crawl_directory_dataset(sample_path, ext_filter=".wav", map_labels=True)
-#     files, true_labels = DirUtils.crawl_directory_dataset(sample_path, ext_filter=".wav")
-#     true_labels = [int(label2id[item]) for item in true_labels]
-#     predictions = []
-#     with torch.no_grad():
-#         for sample_audio_path in tqdm(files):
-#             audio_array = get_audio(sample_audio_path)
-#             audio_array = torch.tensor(audio_array).to(device=device)
-#             audio_array = audio_array[None, ...]
-#             output = model(audio_array)
-#             logits = output["logits"][0]
-#             cls_index = torch.argmax(logits).item()
-#             predictions.append(cls_index)
-#             # print(f"class: {cls_index}, cls_name: {cls_name}")
-#     print("f1_score: ", f1_score(true_labels, predictions, labels=list(true_labels), average="macro"))
-#     print("precision_score: ", precision_score(true_labels, predictions, labels=list(true_labels), average="macro"))
-#     print("accuracy_score: ", accuracy_score(true_labels, predictions))
-#     print("recall_score: ", recall_score(true_labels, predictions, labels=list(true_labels), average="macro"))
+
 
 
 from os.path import join
@@ -119,7 +55,7 @@ def preprocess_function(examples):
 
 if __name__ == '__main__':
     config = Config()
-    best_model_path_ = "results/exp_1/best"
+    best_model_path_ = "results/exp_1/checkpoint-360"
     feature_extractor = AutoFeatureExtractor.from_pretrained(best_model_path_)
     dataset = load_dataset('csv', data_files={'train': config.train_path,
                                               'val': config.val_path, "test": config.test_path})
@@ -171,10 +107,11 @@ if __name__ == '__main__':
         compute_metrics=compute_metrics,
         # optimizers=(optimizer, scheduler)
     )
-
     tic = time()
     # trainer.train()
     print(f"[INFO] Time: {time() - tic}")
     # trainer.save_model(join(config.model_path, config.file_name))
     output = trainer.evaluate(eval_dataset=encoded_dataset["val"])
     print("[INFO] Test evaluation", output)
+    output = trainer.evaluate(eval_dataset=encoded_dataset["train"])
+    print("[INFO] train evaluation", output)
